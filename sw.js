080cf8df7c2241a0d74d86989211ed90fb963654
ygor <ygorrodrigo2@gmail.com>
2026-01-22 10:08:50 -0300

    save icone pwa

---
+++ sw.js
@@ -0,0 +1,45 @@
+// Service Worker básico para PWA
+const CACHE_NAME = 'form-obra-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './topo.png'
+];
+
+// Instalação do service worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Ativação do service worker
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Interceptação de requisições
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Retorna do cache se disponível, senão faz requisição
+        return response || fetch(event.request);
+      })
+  );
+});
